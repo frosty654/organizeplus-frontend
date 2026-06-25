f8414286410f6d41f10e6a3b37d6eb041aaee849
frosty654 <carlosgabrielalmeida065@gmail.com>
2026-06-25 16:56:56 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from '../Layout'
-import Dashboard from '../Dashboard'
-import Metas from '../Metas'
-import Dicas from '../Dicas'
-import Coach from '../Coach'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="metas" element={<Metas />} />
-          <Route path="dicas" element={<Dicas />} />
-          <Route path="coach" element={<Coach />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-)
+import Layout from '/Layout'
+import Dashboard from '/Dashboard'
+import Metas from '/Metas'
+import Dicas from '/Dicas'
+import Coach from '/Coach'
